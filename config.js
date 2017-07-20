3cff8a07522fc7d4097791ada6ed79c6c96d5774
kenpeter <figo2476@hotmail.com>
2017-07-20 12:22:01 +1000

    [change] change back url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {};
 // Port
 config.port = 8014;
 // The Zendesk root end point
-config.backendRootUrl = 'http://resumeback.local';
+config.backendRootUrl = 'https://resumeback.shopshop.space';
 config.backendUserDataUrl = `${config.backendRootUrl}/api/defaultUser`;
 config.backendAuthUrl = `${config.backendRootUrl}/api/auth`;
 config.backendUsername = 'kenpeter';
